32ca9d3a0ade9f2181c7e1d8b82bc87496467d97
barkode <konstantinks@gmail.com>
2023-04-17 13:21:34 +0100

    add elems

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
