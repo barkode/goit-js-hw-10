c3ce082c74d646e1645253a861f884d62e1a8228
barkode <konstantinks@gmail.com>
2023-04-18 23:30:26 +0000

    Deploying to gh-pages from @ barkode/goit-js-hw-10@d1431d366aaba3549925052f0198ff72307a0e96 🚀

--- index.8072f14f.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.8072f14f.js.map
--- index.d6a62265.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");
-//# sourceMappingURL=index.d6a62265.js.map
